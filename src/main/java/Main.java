5198021b39989f7db8809f13e95826b09f7d5606
Faustis1337 <faustis1337@email.com>
2023-03-13 03:15:01 +0100

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,14 +25,48 @@ public class Main {
                 case 2:
                     break;
                 case 3 :
+                    showUpdateDepartmentManager();
                     break;
                 case 4:
+                    showDeleteDepartment();
                     break;
                 case 5:
                     break;
                 case 6:
+                    showGetAllDepartments();
                     break;
             }
         }
     }
+
+    public static void showUpdateDepartmentManager(){
+        System.out.print("Enter department number: ");
+        int departmentNumber = scanner.nextInt();
+        System.out.print("Enter MgrSSN: ");
+        int mgrSSN = scanner.nextInt();
+        if(companyRepository.updateDepartmentManager(departmentNumber,mgrSSN)==1){
+            System.out.println("Manager has been updated!");
+        }else{
+            System.out.println("Failed to update manager...");
+        }
+    }
+
+    public static void showDeleteDepartment(){
+        System.out.print("Enter department number: ");
+        int departmentNumber = scanner.nextInt();
+        if(companyRepository.deleteDepartment(departmentNumber)>0){
+            System.out.println("Successfully deleted department with number: "+departmentNumber);
+        }else{
+            System.out.println("Failed to delete department...");
+        }
+    }
+
+    public static void showGetAllDepartments(){
+        List<Department> allDepartments = companyRepository.getAllDepartments();
+        for(int i = 0; i<allDepartments.size();i++){
+            Department department = allDepartments.get(i);
+            System.out.println(i+1+"| Name: "+department.dName + " No: "+ department.dNumber + " MgrSSN: " + department.mgrSSN + " MgrStartDate: " +department.mgrStartDate + " EmpCount: "+ department.empCount);
+        }
+    }
+
 }
