44f5f9d0ae319b27fe1343c481503a16c3719034
Faustis1337 <faustis1337@email.com>
2023-03-13 02:32:08 +0100

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,36 @@ import dal.ICompanyRepository;
 import dal.database.CompanyRepository;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     private static ICompanyRepository companyRepository = new CompanyRepository();
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        while(true){
+            System.out.println("1: Create Department");
+            System.out.println("2: Update Department Name");
+            System.out.println("3: Update Department Manager");
+            System.out.println("4: Delete Department");
+            System.out.println("5: Get Department");
+            System.out.println("6: Get All Departments");
+            System.out.print("Enter number selection: ");
+
+            int selection = scanner.nextInt();
+            switch(selection){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3 :
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+        }
     }
 }
