156d5af9df62847ce745fceb806b379634e669b2
Faustis1337 <faustis1337@email.com>
2023-03-13 02:11:39 +0100

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,5 @@ import java.util.List;
 public class Main {
     private static ICompanyRepository companyRepository = new CompanyRepository();
     public static void main(String[] args) {
-        System.out.println(companyRepository.updateDepartmentManager(7,888665555));
     }
 }
